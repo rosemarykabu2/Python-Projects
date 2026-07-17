55abd52b83faba07f3dcd1a1b7932721e511943b
Rosemary <rosemarykabu2@gmail.com>
2026-07-17 13:39:13 -0800

    Add Electricity Bill calculator project

---
+++ ELectricity Bill Calculator.py
@@ -0,0 +1,11 @@
+name = "Olivia"
+cost_per_unit = 300
+
+units_consumed =float(input("Enter the number of units consumed: "))
+
+total_bill = cost_per_unit * units_consumed
+
+print(f"Customer name: {name}")
+print(f"Units consumed: {units_consumed}")
+print(f"Cost per unit: {cost_per_unit}")
+print(f"Total bill: {total_bill}")
